87398da20f105c6f7a791106fcd1c31ac29755a9
liyue201 <liyue201@126.com>
2022-07-05 11:39:22 +0800

    a

--- getProof.js
+++ getProof.js
@@ -1,121 +1,129 @@
-const { encode } = require('eth-util-lite')
-const { promisfy } = require('promisfy')
+const {encode} = require('eth-util-lite')
+const {promisfy} = require('promisfy')
 
 const Tree = require('merkle-patricia-tree')
 
-const Rpc  = require('isomorphic-rpc')
+const Rpc = require('isomorphic-rpc')
 const rlp = require('rlp');
-const { arrToBufArr, bufArrToArr } = require ('ethereumjs-util')
+const {arrToBufArr, bufArrToArr} = require('ethereumjs-util')
 
-const { Header, Proof, Receipt, Transaction } = require('eth-object')
+const {Header, Proof, Receipt, Transaction} = require('eth-object')
 
 
-module.exports = class GetProof{
-  constructor(rpcProvider = "https://mainnet.infura.io"){
-    this.rpc = new Rpc(rpcProvider)
-    this.eth_getProof = this.rpc.eth_getProof
-  }
-
-  async transactionProof(txHash){
-    let targetTx = await this.rpc.eth_getTransactionByHash(txHash)
-    if(!targetTx){ throw new Error("Tx not found. Use archive node")}
-
-    let rpcBlock = await this.rpc.eth_getBlockByHash(targetTx.blockHash, true)
-
-    let tree = new Tree();
-
-    await Promise.all(rpcBlock.transactions.map((siblingTx, index) => {
-      let siblingPath = encode(index)
-      let serializedSiblingTx = Transaction.fromRpc(siblingTx).serialize()
-      return promisfy(tree.put, tree)(siblingPath, serializedSiblingTx) 
-    }))
-
-    let [_,__,stack] = await promisfy(tree.findPath, tree)(encode(targetTx.transactionIndex))
-
-    return {
-      header:  Header.fromRpc(rpcBlock),
-      txProof:  Proof.fromStack(stack),
-      txIndex: targetTx.transactionIndex,
+module.exports = class GetProof {
+    constructor(rpcProvider = "https://mainnet.infura.io") {
+        this.rpc = new Rpc(rpcProvider)
+        this.eth_getProof = this.rpc.eth_getProof
     }
-  }
-  async receiptProof(txHash){
-    let targetReceipt = await this.rpc.eth_getTransactionReceipt(txHash)
-    if(!targetReceipt){ throw new Error("txhash/targetReceipt not found. (use Archive node)")}
 
-    console.log("transactionIndex", targetReceipt.transactionIndex)
+    async transactionProof(txHash) {
+        let targetTx = await this.rpc.eth_getTransactionByHash(txHash)
+        if (!targetTx) {
+            throw new Error("Tx not found. Use archive node")
+        }
 
-    let rpcBlock = await this.rpc.eth_getBlockByHash(targetReceipt.blockHash, false)
-    //console.log("rpcBlock", rpcBlock);
+        let rpcBlock = await this.rpc.eth_getBlockByHash(targetTx.blockHash, true)
 
-    let receipts = await Promise.all(rpcBlock.transactions.map((siblingTxHash) => {
-      return this.rpc.eth_getTransactionReceipt(siblingTxHash)
-    }))
+        let tree = new Tree();
 
-    let tree = new Tree();
+        await Promise.all(rpcBlock.transactions.map((siblingTx, index) => {
+            let siblingPath = encode(index)
+            let serializedSiblingTx = Transaction.fromRpc(siblingTx).serialize()
+            return promisfy(tree.put, tree)(siblingPath, serializedSiblingTx)
+        }))
 
-    await Promise.all(receipts.map((siblingReceipt, index) => {
-      console.log("siblingReceipt", siblingReceipt)
-      console.log("index", index)
-      let siblingPath = encode(index)
-      let receipt = Receipt.fromRpc(siblingReceipt);
-      let serializedReceipt = receipt.serialize()
-      console.log("receipt", receipt)
+        let [_, __, stack] = await promisfy(tree.findPath, tree)(encode(targetTx.transactionIndex))
 
-      console.log("siblingPath", siblingPath)
-      //console.log("serializedReceipt", serializedReceipt )
-      return promisfy(tree.put, tree)(siblingPath, serializedReceipt)
-    }))
-
-    let [_,__,stack] = await promisfy(tree.findPath, tree)(encode(targetReceipt.transactionIndex))
-
-    console.log("stack", stack.toString())
-    //let arrayProof = stack.map((trieNode)=>{ return trieNode.raw })
-    //console.log("arrayProof", arrayProof)
-
-    const bufferList = [Buffer.from('123', 'hex'), Buffer.from('456', 'hex')]
-    const proof_blob = rlp.encode(bufArrToArr(bufferList))
-
-    // let proof_blob = rlp.encode([
-    //   2,
-    //   Header.fromRpc(rpcBlock),
-    //   targetReceipt.transactionIndex,
-    //   stack,
-    //])
-    console.log("proof_blob", proof_blob)
-
-    return {
-      header:  Header.fromRpc(rpcBlock),
-      receiptProof:  Proof.fromStack(stack),
-      txIndex: targetReceipt.transactionIndex,
+        return {
+            header: Header.fromRpc(rpcBlock),
+            txProof: Proof.fromStack(stack),
+            txIndex: targetTx.transactionIndex,
+        }
     }
-  }
-  async accountProof(address, blockHash = null){
-    let rpcBlock, rpcProof
-    if(blockHash){
-      rpcBlock = await this.rpc.eth_getBlockByHash(blockHash, false)
-    }else{
-      rpcBlock = await this.rpc.eth_getBlockByNumber('latest', false)
-    }
-    rpcProof = await this.eth_getProof(address, [], rpcBlock.number)
 
-    return {
-      header: Header.fromRpc(rpcBlock),
-      accountProof: Proof.fromRpc(rpcProof.accountProof),
-    }
-  }
-  async storageProof(address, storageAddress, blockHash = null){
-    let rpcBlock, rpcProof
-    if(blockHash){
-      rpcBlock = await this.rpc.eth_getBlockByHash(blockHash, false)
-    }else{
-      rpcBlock = await this.rpc.eth_getBlockByNumber('latest', false)
-    }
-    rpcProof = await this.eth_getProof(address, [storageAddress], rpcBlock.number)
+    async receiptProof(txHash) {
+        let targetReceipt = await this.rpc.eth_getTransactionReceipt(txHash)
+        if (!targetReceipt) {
+            throw new Error("txhash/targetReceipt not found. (use Archive node)")
+        }
 
-    return {
-      header: Header.fromRpc(rpcBlock),
-      accountProof: Proof.fromRpc(rpcProof.accountProof),
-      storageProof: Proof.fromRpc(rpcProof.storageProof[0].proof),
+        console.log("transactionIndex", targetReceipt.transactionIndex)
+
+        let rpcBlock = await this.rpc.eth_getBlockByHash(targetReceipt.blockHash, false)
+        //console.log("rpcBlock", rpcBlock);
+
+        let receipts = await Promise.all(rpcBlock.transactions.map((siblingTxHash) => {
+            return this.rpc.eth_getTransactionReceipt(siblingTxHash)
+        }))
+
+        let tree = new Tree();
+
+        await Promise.all(receipts.map((siblingReceipt, index) => {
+            console.log("siblingReceipt", siblingReceipt)
+            console.log("index", index)
+            let siblingPath = encode(index)
+            let receipt = Receipt.fromRpc(siblingReceipt);
+            let serializedReceipt = receipt.serialize()
+            console.log("receipt", receipt)
+
+            console.log("siblingPath", siblingPath)
+            //console.log("serializedReceipt", serializedReceipt )
+            return promisfy(tree.put, tree)(siblingPath, serializedReceipt)
+        }))
+
+        let [_, __, stack] = await promisfy(tree.findPath, tree)(encode(targetReceipt.transactionIndex))
+
+        console.log("stack", stack.toString())
+        //let arrayProof = stack.map((trieNode)=>{ return trieNode.raw })
+        //console.log("arrayProof", arrayProof)
+
+        //const bufferList = [Buffer.from('123', 'hex'), Buffer.from('456', 'hex')]
+        //const proof_blob = rlp.encode(bufArrToArr(bufferList))
+        const bufferList = [
+            2,
+            Header.fromRpc(rpcBlock),
+            targetReceipt.transactionIndex,
+            stack,
+        ]
+        let proof_blob = rlp.encode(bufArrToArr(bufferList))
+
+        console.log("proof_blob", proof_blob)
+
+        return {
+            header: Header.fromRpc(rpcBlock),
+            receiptProof: Proof.fromStack(stack),
+            txIndex: targetReceipt.transactionIndex,
+        }
     }
-  }
+
+    async accountProof(address, blockHash = null) {
+        let rpcBlock, rpcProof
+        if (blockHash) {
+            rpcBlock = await this.rpc.eth_getBlockByHash(blockHash, false)
+        } else {
+            rpcBlock = await this.rpc.eth_getBlockByNumber('latest', false)
+        }
+        rpcProof = await this.eth_getProof(address, [], rpcBlock.number)
+
+        return {
+            header: Header.fromRpc(rpcBlock),
+            accountProof: Proof.fromRpc(rpcProof.accountProof),
+        }
+    }
+
+    async storageProof(address, storageAddress, blockHash = null) {
+        let rpcBlock, rpcProof
+        if (blockHash) {
+            rpcBlock = await this.rpc.eth_getBlockByHash(blockHash, false)
+        } else {
+            rpcBlock = await this.rpc.eth_getBlockByNumber('latest', false)
+        }
+        rpcProof = await this.eth_getProof(address, [storageAddress], rpcBlock.number)
+
+        return {
+            header: Header.fromRpc(rpcBlock),
+            accountProof: Proof.fromRpc(rpcProof.accountProof),
+            storageProof: Proof.fromRpc(rpcProof.storageProof[0].proof),
+        }
+    }
 }
